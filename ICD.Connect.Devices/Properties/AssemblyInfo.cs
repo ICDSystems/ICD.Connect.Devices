aa6d2ab56053a41621ed3b7143fcffcd8d98d182
Chris Cameron <chris.cameron@wedoresi.com>
2017-06-26 11:36:37 -0400

    Updating KrangPlugin usages, moving into a new file for net standard

--- ICD.Connect.Devices/Properties/AssemblyInfo.cs
+++ ICD.Connect.Devices/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
-using ICD.Common.Attributes;
 
-[assembly: KrangPlugin]
 [assembly: AssemblyTitle("ICD.Connect.Devices")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ICD.Connect.Devices")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyVersion("1.0.0.*")]
-
