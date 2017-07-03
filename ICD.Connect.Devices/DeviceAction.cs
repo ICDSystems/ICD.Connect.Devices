eaae2c286c4e8f582bff4d2858fb12e5a1f7c504
jeff.thompson <jeffery.thompson@wedoresi.com>
2017-07-03 09:57:02 -0400

    DeviceAction for permissions

---
+++ ICD.Connect.Devices/DeviceAction.cs
@@ -0,0 +1,13 @@
+﻿using ICD.Connect.Settings;
+
+namespace ICD.Connect.Devices
+{
+	public class DeviceAction : OriginatorAction
+	{
+		protected DeviceAction(string val) : base(val)
+		{
+		}
+
+		public static DeviceAction ViewControls { get { return new DeviceAction("DeviceAction.ViewControls"); } }
+	}
+}
