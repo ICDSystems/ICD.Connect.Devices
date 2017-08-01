8a4c971b2c7633d8dc38379f0f1a0f3d8f665c12
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-01 14:47:41 -0400

    Initial commit of IPowerDeviceControl and abstraction

---
+++ ICD.Connect.Devices/Controls/AbstractPowerDeviceControl.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+
+namespace ICD.Connect.Devices.Controls
+{
+	public abstract class AbstractPowerDeviceControl<TDevice> : AbstractDeviceControl<TDevice>, IPowerDeviceControl
+		where TDevice : IDevice
+	{
+		/// <summary>
+		/// Raised when the powered state changes.
+		/// </summary>
+		public event EventHandler<BoolEventArgs> OnIsPoweredChanged;
+
+		private bool m_IsPowered;
+
+		/// <summary>
+		/// Gets the powered state of the device.
+		/// </summary>
+		public bool IsPowered
+		{
+			get { return m_IsPowered; }
+			protected set
+			{
+				if (value == m_IsPowered)
+					return;
+
+				m_IsPowered = value;
+
+				OnIsPoweredChanged.Raise(this, new BoolEventArgs(m_IsPowered));
+			}
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		protected AbstractPowerDeviceControl(TDevice parent, int id)
+			: base(parent, id)
+		{
+		}
+
+		/// <summary>
+		/// Override to release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnIsPoweredChanged = null;
+
+			base.DisposeFinal(disposing);
+		}
+
+		#region Methods
+
+		/// <summary>
+		/// Powers on the device.
+		/// </summary>
+		public abstract void PowerOn();
+
+		/// <summary>
+		/// Powers off the device.
+		/// </summary>
+		public abstract void PowerOff();
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("Powered", IsPowered);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new ConsoleCommand("PowerOn", "Turns on the device", () => PowerOn());
+			yield return new ConsoleCommand("PowerOff", "Turns off the device", () => PowerOff());
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Devices/Controls/IPowerDeviceControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ICD.Common.EventArguments;
+
+namespace ICD.Connect.Devices.Controls
+{
+	public interface IPowerDeviceControl : IDeviceControl
+	{
+		/// <summary>
+		/// Raised when the powered state changes.
+		/// </summary>
+		event EventHandler<BoolEventArgs> OnIsPoweredChanged;
+
+		/// <summary>
+		/// Gets the powered state of the device.
+		/// </summary>
+		bool IsPowered { get; }
+
+		/// <summary>
+		/// Powers on the device.
+		/// </summary>
+		void PowerOn();
+
+		/// <summary>
+		/// Powers off the device.
+		/// </summary>
+		void PowerOff();
+	}
+}
