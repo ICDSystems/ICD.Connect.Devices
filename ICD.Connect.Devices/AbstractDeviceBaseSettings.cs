3a83aca38a4f11846111ca8f476b9bc6c8e902f7
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-11-10 14:16:44 -0500

    Optimize bad dependency checking to allow device removal from settings in a timely manner.

--- ICD.Connect.Devices/AbstractDeviceBaseSettings.cs
+++ ICD.Connect.Devices/AbstractDeviceBaseSettings.cs
@@ -6,18 +6,6 @@ namespace ICD.Connect.Settings
 	public abstract class AbstractDeviceBaseSettings : AbstractSettings, IDeviceBaseSettings
 	{
 		/// <summary>
-		/// Returns the collection of ids that the settings will depend on.
-		/// For example, to instantiate an IR Port from settings, the device the physical port
-		/// belongs to will need to be instantiated first.
-		/// </summary>
-		/// <returns></returns>
-		public override IEnumerable<int> GetDeviceDependencies()
-		{
-			// Typically a device doesn't depend directly on other devices.
-			yield break;
-		}
-
-		/// <summary>
 		/// Parses the xml and applies the properties to the instance.
 		/// </summary>
 		/// <param name="instance"></param>
