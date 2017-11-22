65f5d1963aec719d9f83d4e2d570f32bc8b37f75
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-22 10:09:47 -0500

    Renaming Action to Permissable

---
+++ ICD.Connect.Devices/DevicePermissable.cs
@@ -0,0 +1,13 @@
+﻿using ICD.Connect.Settings;
+
+namespace ICD.Connect.Devices
+{
+	public class DevicePermissable : OriginatorPermissable
+	{
+		protected DevicePermissable(string val) : base(val)
+		{
+		}
+
+		public static DevicePermissable ViewControls { get { return new DevicePermissable("DeviceAction.ViewControls"); } }
+	}
+}
