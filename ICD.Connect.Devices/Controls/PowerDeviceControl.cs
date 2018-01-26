41e224176f8e1472eed46f9b28419b36784b4451
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-01-26 14:26:09 -0500

    inital commit for cameras branch, add concrete PowerDeviceControl and IDeviceWithPower

---
+++ ICD.Connect.Devices/Controls/PowerDeviceControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICD.Connect.Devices.Controls
+{
+    public class PowerDeviceControl<T> : AbstractPowerDeviceControl<T>
+		where T : IDeviceWithPower
+    {
+	    public PowerDeviceControl(T parent, int id) : base(parent, id)
+	    {
+	    }
+
+	    public override void PowerOn()
+	    {
+		    Parent.PowerOn();
+	    }
+
+	    public override void PowerOff()
+	    {
+		    Parent.PowerOff();
+	    }
+    }
+}
