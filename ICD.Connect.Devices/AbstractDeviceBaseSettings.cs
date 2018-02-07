3ceb9ecbe817b52989feb67a2bc7c0b14bbf6b17
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-07 13:52:40 -0500

    Removing unused methods

--- ICD.Connect.Devices/AbstractDeviceBaseSettings.cs
+++ ICD.Connect.Devices/AbstractDeviceBaseSettings.cs
@@ -4,14 +4,5 @@ namespace ICD.Connect.Devices
 {
 	public abstract class AbstractDeviceBaseSettings : AbstractSettings, IDeviceBaseSettings
 	{
-		/// <summary>
-		/// Parses the xml and applies the properties to the instance.
-		/// </summary>
-		/// <param name="instance"></param>
-		/// <param name="xml"></param>
-		protected static void ParseXml(AbstractDeviceBaseSettings instance, string xml)
-		{
-			AbstractSettings.ParseXml(instance, xml);
-		}
 	}
 }
--- ICD.Connect.Devices/AbstractDeviceSettings.cs
+++ ICD.Connect.Devices/AbstractDeviceSettings.cs
@@ -8,15 +8,5 @@
 		/// Gets the xml element.
 		/// </summary>
 		protected override string Element { get { return DEVICE_ELEMENT; } }
-
-		/// <summary>
-		/// Parses the xml and applies the properties to the instance.
-		/// </summary>
-		/// <param name="instance"></param>
-		/// <param name="xml"></param>
-		protected static void ParseXml(AbstractDeviceSettings instance, string xml)
-		{
-			AbstractDeviceBaseSettings.ParseXml(instance, xml);
-		}
 	}
 }
