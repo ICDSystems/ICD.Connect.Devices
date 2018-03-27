2824e4ce56b4e7a888334e2c07ee5451239c25de
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-27 16:11:35 -0400

    Reducing duplicate code

--- ICD.Connect.Devices/Proxies/ProxyDeviceBase.cs
+++ ICD.Connect.Devices/Proxies/ProxyDeviceBase.cs
@@ -1,21 +1,6 @@
-﻿using System;
-
-namespace ICD.Connect.Devices.Proxies
+﻿namespace ICD.Connect.Devices.Proxies
 {
-    public sealed class ProxyDeviceBase : AbstractDeviceBase<ProxyDeviceBaseSettings>
+    public sealed class ProxyDeviceBase : AbstractProxyDeviceBase
     {
-		protected override bool GetIsOnlineStatus()
-		{
-			return true;
-		}
-	}
-
-	public sealed class ProxyDeviceBaseSettings : AbstractDeviceBaseSettings
-	{
-		protected override string Element { get { return null; } }
-
-		public override string FactoryName { get { return null; } }
-
-		public override Type OriginatorType { get { return null; } }
 	}
 }
