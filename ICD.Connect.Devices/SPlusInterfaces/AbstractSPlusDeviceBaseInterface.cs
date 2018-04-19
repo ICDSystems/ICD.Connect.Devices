e9c52fae0589f6cba801c82e2b5dba2a4301fe90
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-19 11:47:00 -0400

    feat: Adding SPlus device interface abstractions

---
+++ ICD.Connect.Devices/SPlusInterfaces/AbstractSPlusDeviceBaseInterface.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Devices.Simpl;
+using ICD.Connect.Settings.SPlusInterfaces;
+
+namespace ICD.Connect.Devices.SPlusInterfaces
+{
+	public abstract class AbstractSPlusDeviceBaseInterface<TOriginator> : AbstractSPlusOriginatorInterface<TOriginator>
+		where TOriginator : ISimplDeviceBase
+	{
+	}
+}
---
+++ ICD.Connect.Devices/SPlusInterfaces/AbstractSPlusDeviceInterface.cs
@@ -0,0 +1,9 @@
+﻿using ICD.Connect.Devices.Simpl;
+
+namespace ICD.Connect.Devices.SPlusInterfaces
+{
+	public abstract class AbstractSPlusDeviceInterface<TOriginator> : AbstractSPlusDeviceBaseInterface<TOriginator>
+		where TOriginator : ISimplDevice
+	{
+	}
+}
