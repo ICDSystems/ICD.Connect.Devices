2102050f1c0e4ebbdb526b9bff59a4e16bdf20ce
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-02 13:51:53 -0400

    refactor: Moved volume controls and volume repeaters into Audio project

--- ICD.Connect.Devices/Controls/IVolumeDeviceControl.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ICD.Connect.Devices.Controls
-{
-	/// <summary>
-	/// This interface just acts as a roll-up for <see cref="IVolumeLevelBasicDeviceControl"/> and <see cref="IVolumeMuteBasicDeviceControl"/>
-	/// </summary>
-	public interface IVolumeDeviceControl : IDeviceControl
-	{
-	}
-}
