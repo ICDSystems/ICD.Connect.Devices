bf2e209d4c69e4548ad4fba4c8f6f5d1e381ade1
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-04 09:55:26 -0400

    refactor: Settings element is now provided by parent

--- ICD.Connect.Devices/Devices/Simpl/AbstractSimplDeviceSettings.cs
+++ ICD.Connect.Devices/Devices/Simpl/AbstractSimplDeviceSettings.cs
@@ -2,9 +2,5 @@
 {
 	public abstract class AbstractSimplDeviceSettings : AbstractSimplDeviceBaseSettings, ISimplDeviceSettings
 	{
-		/// <summary>
-		/// Gets the xml element.
-		/// </summary>
-		protected override string Element { get { return AbstractDeviceSettings.DEVICE_ELEMENT; } }
 	}
 }
