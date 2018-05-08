38cbd6f58639d69bfd4237a6cdd5bffd23a1a51c
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-05-08 17:22:42 -0400

    refactor: rename ambiguous s+interface to shim

--- ICD.Connect.Devices/SPlusInterfaces/AbstractSPlusDeviceInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using ICD.Connect.Devices.Simpl;
-
-namespace ICD.Connect.Devices.SPlusInterfaces
-{
-	public abstract class AbstractSPlusDeviceInterface<TOriginator> : AbstractSPlusDeviceBaseInterface<TOriginator>
-		where TOriginator : ISimplDevice
-	{
-	}
-}
---
+++ ICD.Connect.Devices/SPlusShims/AbstractSPlusDeviceShim.cs
@@ -0,0 +1,9 @@
+﻿using ICD.Connect.Devices.Simpl;
+
+namespace ICD.Connect.Devices.SPlusShims
+{
+	public abstract class AbstractSPlusDeviceShim<TOriginator> : AbstractSPlusDeviceBaseShim<TOriginator>
+		where TOriginator : ISimplDevice
+	{
+	}
+}
