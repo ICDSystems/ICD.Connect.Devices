eebbb1a46ae684a516afae2f4f622297c84a42f3
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-24 17:08:31 -0400

    docs: Adding missing comments

--- ICD.Connect.Devices/Devices/IDeviceWithPower.cs
+++ ICD.Connect.Devices/Devices/IDeviceWithPower.cs
@@ -2,8 +2,14 @@
 {
 	public interface IDeviceWithPower : IDevice
 	{
+		/// <summary>
+		/// Powers on the device.
+		/// </summary>
 		void PowerOn();
 
+		/// <summary>
+		/// Powers off the device.
+		/// </summary>
 		void PowerOff();
 	}
 }
