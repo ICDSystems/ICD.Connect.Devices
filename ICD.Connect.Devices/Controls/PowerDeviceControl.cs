c02a81e978cd86214bf320bb8f94a8fbbd6e9fbf
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-07-01 12:07:12 -0400

    chore: tidying

--- ICD.Connect.Devices/Controls/PowerDeviceControl.cs
+++ ICD.Connect.Devices/Controls/PowerDeviceControl.cs
@@ -1,20 +1,18 @@
 ﻿namespace ICD.Connect.Devices.Controls
 {
     public sealed class PowerDeviceControl<T> : AbstractPowerDeviceControl<T>
-		where T : IDeviceWithPower
+        where T : IDeviceWithPower
     {
-	    public PowerDeviceControl(T parent, int id) : base(parent, id)
-	    {
-	    }
+        public PowerDeviceControl(T parent, int id) : base(parent, id) { }
 
-	    public override void PowerOn()
-	    {
-		    Parent.PowerOn();
-	    }
+        public override void PowerOn()
+        {
+            Parent.PowerOn();
+        }
 
-	    public override void PowerOff()
-	    {
-		    Parent.PowerOff();
-	    }
-	}
+        public override void PowerOff()
+        {
+            Parent.PowerOff();
+        }
+    }
 }
