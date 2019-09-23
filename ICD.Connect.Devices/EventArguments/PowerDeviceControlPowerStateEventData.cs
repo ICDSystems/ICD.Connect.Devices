4a303986e86e32240e9bcf2fba4a61d693d48cdc
Drew Tingen <drew.tingen@icdsystems.com>
2019-09-23 12:02:54 -0400

    fix: Data class is serializable

--- ICD.Connect.Devices/EventArguments/PowerDeviceControlPowerStateEventData.cs
+++ ICD.Connect.Devices/EventArguments/PowerDeviceControlPowerStateEventData.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using ICD.Connect.Devices.Controls;
 
 namespace ICD.Connect.Devices.EventArguments
 {
+	[Serializable]
 	public sealed class PowerDeviceControlPowerStateEventData
 	{
 
