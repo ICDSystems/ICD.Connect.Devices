c17577744757335019dca8b79787da1f6d8d074f
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-31 17:49:01 -0400

    refactor: Removing unused code

--- ICD.Connect.Devices/Controls/IDeviceControl.cs
+++ ICD.Connect.Devices/Controls/IDeviceControl.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using ICD.Common.Logging.LoggingContexts;
-using ICD.Common.Properties;
 using ICD.Common.Utils;
-using ICD.Common.Utils.Extensions;
 using ICD.Connect.API.Attributes;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Devices.EventArguments;
@@ -79,97 +75,6 @@ namespace ICD.Connect.Devices.Controls
 				throw new ArgumentNullException("extends");
 
 			return new DeviceControlInfo(extends.Parent.Id, extends.Id);
-		}
-
-		/// <summary>
-		/// Gets the sibling control with the given id.
-		/// </summary>
-		/// <param name="extends"></param>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		[NotNull]
-		[PublicAPI]
-		public static IDeviceControl GetSibling(this IDeviceControl extends, int id)
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			return extends.Parent
-			              .Controls
-			              .GetControl(id);
-		}
-
-		/// <summary>
-		/// Gets the first sibling control of the given type.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="extends"></param>
-		/// <returns></returns>
-		[NotNull]
-		[PublicAPI]
-		public static T GetSibling<T>(this IDeviceControl extends)
-			where T : IDeviceControl
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			return extends.GetSiblings()
-			              .Except(extends)
-			              .OfType<T>()
-			              .First();
-		}
-
-		/// <summary>
-		/// Gets the sibling control with the given id and type.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="extends"></param>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		[NotNull]
-		[PublicAPI]
-		public static T GetSibling<T>(this IDeviceControl extends, int id)
-			where T : class, IDeviceControl
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			return extends.Parent
-			              .Controls
-			              .GetControl<T>(id);
-		}
-
-		/// <summary>
-		/// Gets all of the controls on the parent device except this control.
-		/// </summary>
-		/// <param name="extends"></param>
-		/// <returns></returns>
-		[NotNull]
-		[PublicAPI]
-		public static IEnumerable<IDeviceControl> GetSiblings(this IDeviceControl extends)
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			return extends.Parent
-			              .Controls
-			              .Except(extends);
-		}
-
-		/// <summary>
-		/// Gets all of the controls of the given type on the parent device except this control.
-		/// </summary>
-		/// <param name="extends"></param>
-		/// <returns></returns>
-		[NotNull]
-		[PublicAPI]
-		public static IEnumerable<T> GetSiblings<T>(this IDeviceControl extends)
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			return extends.GetSiblings()
-			              .OfType<T>();
 		}
 	}
 }
