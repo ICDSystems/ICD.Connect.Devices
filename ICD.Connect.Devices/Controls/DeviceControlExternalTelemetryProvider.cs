85caa6b3cad4855ef9a508952f973da8627f8684
Chris Cameron <chris.cameron@wedoresi.com>
2020-07-07 14:32:40 -0400

    refactor: Simplifying external telemetry provider

---
+++ ICD.Connect.Devices/Controls/DeviceControlExternalTelemetryProvider.cs
@@ -0,0 +1,12 @@
+﻿using ICD.Common.Utils.Extensions;
+using ICD.Connect.Telemetry.Attributes;
+using ICD.Connect.Telemetry.Providers.External;
+
+namespace ICD.Connect.Devices.Controls
+{
+	public sealed class DeviceControlExternalTelemetryProvider : AbstractExternalTelemetryProvider<IDeviceControl>
+	{
+		[PropertyTelemetry("DeviceControlType", null, null)]
+		public string DeviceControlType { get { return Parent.GetType().GetMinimalName(); } }
+	}
+}
