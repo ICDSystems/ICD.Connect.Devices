4d6d8602813679f9652e0513f239e988e5304123
Drew Tingen <drew.tingen@icdsystems.com>
2021-01-15 00:33:25 -0500

    chore: update changelog, increment assembly version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [14.0.0] - 2021-01-14
 ### Added
  - Added RoomCritical configuration item to devices for specifying that a device is critical to a rooms operation
 
--- ICD.Connect.Devices.CrestronSPlus/Properties/AssemblyInfo.cs
+++ ICD.Connect.Devices.CrestronSPlus/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Devices.CrestronSPlus")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("12.0.1.0")]
+[assembly: AssemblyVersion("14.0.0.0")]
 
