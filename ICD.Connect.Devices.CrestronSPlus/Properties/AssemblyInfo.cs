b5a361a366fedf8522a5b8a8359196eedb6092b6
Drew Tingen <drew.tingen@icdsystems.com>
2022-07-01 12:38:23 -0400

    chore: Update changelog, increment assembly patch version

--- ICD.Connect.Devices.CrestronSPlus/Properties/AssemblyInfo.cs
+++ ICD.Connect.Devices.CrestronSPlus/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Devices.CrestronSPlus")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
-[assembly: AssemblyVersion("15.0.1.0")]
+[assembly: AssemblyVersion("15.0.2.0")]
