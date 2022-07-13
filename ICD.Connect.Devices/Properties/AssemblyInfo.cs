3a5a3b75d4bfa38add0c60b5c2364ba752743643
Drew Tingen <drew.tingen@icdsystems.com>
2022-07-13 12:16:37 -0400

    chore: Update changelog, increment assembly patch version

--- ICD.Connect.Devices/Properties/AssemblyInfo.cs
+++ ICD.Connect.Devices/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Devices")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
-[assembly: AssemblyVersion("15.0.2.0")]
+[assembly: AssemblyVersion("15.0.3.0")]
